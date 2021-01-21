878016afa37bdb487c961cf3d6b9995a4d2480ac
Mark Pasricha <markp862@gmail.com>
2021-01-21 15:28:39 -0500

    adding in 5 day

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,13 @@
 var APIKey = "2b4f7000607c6ae8526e73b01a82fcae";
 var today = moment().format("MMMM Do YYYY");
 
+function convertToF(temp){
+	return Math.floor((temp - 273.15) * 1.80 + 32);
+}
 function getUVData(lat, lon){
 	var queryURL = `http://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${APIKey}`
 
-	return $.ajax({
+	$.ajax({
 		url: queryURL, 
 		method: "GET", 
 		success: function(response){
@@ -19,17 +22,29 @@ function getOneDayData(city){
       	url: queryURL,
       	method: "GET", 
       	success: function(response){
-      	var latCoord = response.coord.lat; 
-    	var lonCoord = response.coord.lon;
+	      	var latCoord = response.coord.lat; 
+	    	var lonCoord = response.coord.lon;
 
-    	getUVData(latCoord, lonCoord);
+	    	getUVData(latCoord, lonCoord);
 
-    	$("#city").html(`${response.name} ${today} <img src=https://openweathermap.org/img/w/${response.weather[0].icon}.png alt=WeatherIcon>`);
-    	$("#temp").html(`Temperature: ${Math.floor((response.main.temp - 273.15) * 1.80 + 32)}&#176;F`);
-    	$("#humid").text(`Humidity: ${response.main.humidity}%`); 
-    	$("#wind").text(`Wind Speed: ${response.wind.speed} MPH`); 
+	    	$("#city").html(`${response.name} ${today} <img src=https://openweathermap.org/img/w/${response.weather[0].icon}.png alt=WeatherIcon>`);
+	    	$("#temp").html(`Temperature: ${convertToF(response.main.temp)}&#176;F`);
+	    	$("#humid").text(`Humidity: ${response.main.humidity}%`); 
+	    	$("#wind").text(`Wind Speed: ${response.wind.speed} MPH`); 
       	}
     });
+}
+
+function getFiveDayData(city){
+	var queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APIKey}`
+
+	$.ajax({
+		url:queryURL, 
+		method: "GET", 
+		success: function(response){
+			console.log(response);
+		}
+	})
 }
 
 $(".searchbtn").on("click", function(){
@@ -37,19 +52,6 @@ $(".searchbtn").on("click", function(){
 	$("#cityname").val("");
 
 	getOneDayData(city);
-	
+	getFiveDayData(city);
 
-	// $.ajax({
- //      url: queryURL,
- //      method: "GET"
- //    }).then(function(response) {
- //    	console.log(response)
- //    	var latCoord = response.coord.lat; 
- //    	var lonCoord = response.coord.lon;
-
- //    	$("#city").html(`${response.name} ${today} <img src=https://openweathermap.org/img/w/${response.weather[0].icon}.png alt=WeatherIcon>`);
- //    	$("#temp").html(`Temperature: ${Math.floor((response.main.temp - 273.15) * 1.80 + 32)}&#176;F`);
- //    	$("#humid").text(`Humidity: ${response.main.humidity}%`); 
- //    	$("#wind").text(`Wind Speed: ${response.wind.speed} MPH`); 
- //    });
 })
