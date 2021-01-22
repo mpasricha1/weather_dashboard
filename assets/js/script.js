d5e00291b21722356cbb78764bb155c75d0b5dd4
Mark Pasricha <markp862@gmail.com>
2021-01-21 20:18:20 -0500

    finished 5 day

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,17 +2,17 @@ var APIKey = "2b4f7000607c6ae8526e73b01a82fcae";
 var today = moment().format("MMMM Do YYYY");
 
 function convertToF(temp){
-	return Math.floor((temp - 273.15) * 1.80 + 32)
-}
+	return Math.floor((temp - 273.15) * 1.80 + 32);
+};
 function convertDate(date){
 	var newDate = new Date(0); 
-	newDate.setUTCSeconds(date)
-	newDate = newDate.toDateString()
+	newDate.setUTCSeconds(date);
+	newDate = newDate.toDateString();
 
 	return newDate;
-}
+};
 function getUVData(lat, lon){
-	var queryURL = `http://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${APIKey}`
+	var queryURL = `http://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${APIKey}`;
 
 	$.ajax({
 		url: queryURL, 
@@ -25,10 +25,10 @@ function getUVData(lat, lon){
 				$("#uv").css({"background-color": "orange", "color": "white"});
 			}else{
 				$("#uv").css({"background-color": "red", "color": "white"});
-			}
-		}
+			};
+		};
 	});
-}
+};
 
 function getOneDayData(city){
 	var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
@@ -39,16 +39,15 @@ function getOneDayData(city){
 	      	var latCoord = response.coord.lat; 
 	    	var lonCoord = response.coord.lon;
 
-	    	console.log(response)
 	    	getUVData(latCoord, lonCoord);
 
 	    	$("#city").html(`${response.name} ${convertDate(response.dt)} <img src=https://openweathermap.org/img/w/${response.weather[0].icon}.png alt=WeatherIcon>`);
 	    	$("#temp").html(`Temperature: ${convertToF(response.main.temp)}&#176;F`);
 	    	$("#humid").text(`Humidity: ${response.main.humidity}%`); 
 	    	$("#wind").text(`Wind Speed: ${response.wind.speed} MPH`); 
-      	}
+      	};
     });
-}
+};
 
 function getFiveDayData(city){
 	var queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APIKey}`
@@ -66,18 +65,17 @@ function getFiveDayData(city){
 					$(`#temp${idCounter}`).html(`Temperature: ${convertToF(response.list[i].main.temp)}&#176;F`);
 					$(`#humid${idCounter}`).text(`Humidity: ${response.list[i].main.humidity}%`); 
 					idCounter++;
-				}	
-			}
-		}
-	})
-}
+				};
+			};
+		};
+	});
+};
 
 $(".searchbtn").on("click", function(){
 	var city = $("#cityname").val();
 	$("#cityname").val("");
 	var listItem = $("<button>").attr({"class": "list-group-item list-group-item-action prevsearch", "type": "button"}).text(city)
 	$(".list-group-flush").prepend(listItem);
-
 
 	getOneDayData(city);
 	getFiveDayData(city);
@@ -87,4 +85,5 @@ $(".searchbtn").on("click", function(){
 $(document).on("click", ".prevsearch", function(){
 	var city = $(this).text();
 	getOneDayData(city);
+	getFiveDayData(city);
 });
