73f3e57e364cb2d05d8ba8d7946dbef1bb6b32ee
Mark Pasricha <markp862@gmail.com>
2021-01-21 20:02:33 -0500

    finished 5 day forcast

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,13 @@ var today = moment().format("MMMM Do YYYY");
 function convertToF(temp){
 	return Math.floor((temp - 273.15) * 1.80 + 32)
 }
+function convertDate(date){
+	var newDate = new Date(0); 
+	newDate.setUTCSeconds(date)
+	newDate = newDate.toDateString()
+
+	return newDate;
+}
 function getUVData(lat, lon){
 	var queryURL = `http://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${APIKey}`
 
@@ -32,9 +39,10 @@ function getOneDayData(city){
 	      	var latCoord = response.coord.lat; 
 	    	var lonCoord = response.coord.lon;
 
+	    	console.log(response)
 	    	getUVData(latCoord, lonCoord);
 
-	    	$("#city").html(`${response.name} ${today} <img src=https://openweathermap.org/img/w/${response.weather[0].icon}.png alt=WeatherIcon>`);
+	    	$("#city").html(`${response.name} ${convertDate(response.dt)} <img src=https://openweathermap.org/img/w/${response.weather[0].icon}.png alt=WeatherIcon>`);
 	    	$("#temp").html(`Temperature: ${convertToF(response.main.temp)}&#176;F`);
 	    	$("#humid").text(`Humidity: ${response.main.humidity}%`); 
 	    	$("#wind").text(`Wind Speed: ${response.wind.speed} MPH`); 
@@ -49,7 +57,17 @@ function getFiveDayData(city){
 		url:queryURL, 
 		method: "GET", 
 		success: function(response){
-			console.log(response);
+			var idCounter = 1; 
+			for(var i=0; i<response.list.length; i++){
+				if(response.list[i].dt_txt.includes("12:00:00")){
+					var date = convertDate(response.list[i].dt)
+					$(`#date${idCounter}`).text(date);
+					$(`#img${idCounter}`).attr({"src": `https://openweathermap.org/img/w/${response.list[i].weather[0].icon}.png`, "alt": "WeatherIcon"})
+					$(`#temp${idCounter}`).html(`Temperature: ${convertToF(response.list[i].main.temp)}&#176;F`);
+					$(`#humid${idCounter}`).text(`Humidity: ${response.list[i].main.humidity}%`); 
+					idCounter++;
+				}	
+			}
 		}
 	})
 }
@@ -67,5 +85,6 @@ $(".searchbtn").on("click", function(){
 }); 
 
 $(document).on("click", ".prevsearch", function(){
-	getOneDayData($(this).text());
+	var city = $(this).text();
+	getOneDayData(city);
 });
