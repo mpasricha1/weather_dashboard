31b90dce80d1dfa99af1e2ffebb19db7b77f3b22
Mark Pasricha <markp862@gmail.com>
2021-01-21 19:07:23 -0500

    added recent search functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ var APIKey = "2b4f7000607c6ae8526e73b01a82fcae";
 var today = moment().format("MMMM Do YYYY");
 
 function convertToF(temp){
-	return Math.floor((temp - 273.15) * 1.80 + 32);
+	return Math.floor((temp - 273.15) * 1.80 + 32)
 }
 function getUVData(lat, lon){
 	var queryURL = `http://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${APIKey}`
@@ -12,6 +12,13 @@ function getUVData(lat, lon){
 		method: "GET", 
 		success: function(response){
 			$("#uv").text(`UV Index:${response.value}`);
+			if(response.value < 2){
+				$("#uv").css({"background-color": "green", "color": "white"});
+			}else if(response.value < 7){
+				$("#uv").css({"background-color": "orange", "color": "white"});
+			}else{
+				$("#uv").css({"background-color": "red", "color": "white"});
+			}
 		}
 	});
 }
@@ -50,8 +57,15 @@ function getFiveDayData(city){
 $(".searchbtn").on("click", function(){
 	var city = $("#cityname").val();
 	$("#cityname").val("");
+	var listItem = $("<button>").attr({"class": "list-group-item list-group-item-action prevsearch", "type": "button"}).text(city)
+	$(".list-group-flush").prepend(listItem);
+
 
 	getOneDayData(city);
 	getFiveDayData(city);
 
-})
+}); 
+
+$(document).on("click", ".prevsearch", function(){
+	getOneDayData($(this).text());
+});
